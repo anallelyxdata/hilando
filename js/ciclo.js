213e5ce21347a0c111fa000e80fe6a93c8eeb987
anallelyxdata <anallely@x-data.mx>
2024-01-27 23:39:05 -0600

    listo hasta sección de caleidoscopio y tamaño max 1400px

--- js/ciclo.js
+++ js/ciclo.js
@@ -23,11 +23,16 @@ var textPathRotorElements = document.querySelectorAll(".textPath-rotor");
 
 document.getElementById("aux-fibra").addEventListener("click", function() {
     
-      var offsetFibra = -410; // Adjust this value based on your requirement
+      var offsetPercentageFibra = 10; // 10% of the viewport height
+
+      // Calculate the offset based on the viewport height
+      var viewportHeightFibra = window.innerHeight;
+      var offsetFibra = -(viewportHeightFibra * offsetPercentageFibra / 100);
+
       var targetScrollPositionFibra = document.getElementById("ciclo_aux_fibra").getBoundingClientRect().top + window.scrollY + offsetFibra;
 
       window.scrollTo({ top: targetScrollPositionFibra, behavior: 'smooth' });
-
+      alert('fibra')
    
 });
 
@@ -36,10 +41,24 @@ var textPathDis = document.getElementById("textpath-dis");
 
 document.getElementById("aux-dis").addEventListener("click", function() {
 
-      var offsetDis = -410; // Adjust this value based on your requirement
+      // var offsetDis = -410; // Adjust this value based on your requirement
+      // var targetScrollPositionDis = document.getElementById("ciclo_aux_dis").getBoundingClientRect().top + window.scrollY + offsetDis;
+
+      // window.scrollTo({ top: targetScrollPositionDis, behavior: 'smooth' });
+      // console.log('diseño');
+
+      // Adjust this percentage based on your requirement
+      var offsetPercentageDis = 25; // 10% of the viewport height
+
+      // Calculate the offset based on the viewport height
+      var viewportHeightDis = window.innerHeight;
+      var offsetDis = -(viewportHeightDis * offsetPercentageDis / 100);
+
       var targetScrollPositionDis = document.getElementById("ciclo_aux_dis").getBoundingClientRect().top + window.scrollY + offsetDis;
 
       window.scrollTo({ top: targetScrollPositionDis, behavior: 'smooth' });
+      alert('dis')
+
 
 });
 
@@ -50,11 +69,17 @@ var textPathManufactura = document.getElementById("textpath-manufactura");
 document.getElementById("aux-manufactura").addEventListener("click", function() {
     
 
-      var offsetMan = -410; // Adjust this value based on your requirement
-      var targetScrollPositionMan = document.getElementById("ciclo_aux_manufactura").getBoundingClientRect().top + window.scrollY + offsetMan;
+    var offsetPercentageMan = 10; // 10% of the viewport height
 
-      window.scrollTo({ top: targetScrollPositionMan, behavior: 'smooth' });
+    // Calculate the offset based on the viewport height
+    var viewportHeightMan = window.innerHeight;
+    var offsetMan = -(viewportHeightMan * offsetPercentageMan / 100);
 
+    var targetScrollPositionMan = document.getElementById("ciclo_aux_manufactura").getBoundingClientRect().top + window.scrollY + offsetMan;
+
+    window.scrollTo({ top: targetScrollPositionMan, behavior: 'smooth' });
+
+      alert('man')
 
 });
 
@@ -64,11 +89,17 @@ var textPathLogistica = document.getElementById("textpath-logistica");
 // Add a mouseout event listener to the element with the ID "aux-fibra"
 document.getElementById("aux-logistica").addEventListener("click", function() {
    
-    var offsetLogistica = -410; // Adjust this value based on your requirement
+    var offsetPercentageLogistica = 10; // 10% of the viewport height
+
+    // Calculate the offset based on the viewport height
+    var viewportHeightLogistica = window.innerHeight;
+    var offsetLogistica = -(viewportHeightLogistica * offsetPercentageLogistica / 100);
+
     var targetScrollPositionLogistica = document.getElementById("ciclo_aux_logistica").getBoundingClientRect().top + window.scrollY + offsetLogistica;
 
     window.scrollTo({ top: targetScrollPositionLogistica, behavior: 'smooth' });
 
+      alert('log')
    
 });
 
@@ -79,12 +110,18 @@ var textPathUso = document.getElementById("textpath-uso");
 document.getElementById("aux-uso").addEventListener("click", function() {
    
 
-    var offsetUso = -410; // Adjust this value based on your requirement
+    var offsetPercentageUso = 10; // 10% of the viewport height
+
+    // Calculate the offset based on the viewport height
+    var viewportHeightUso = window.innerHeight;
+    var offsetUso = -(viewportHeightUso * offsetPercentageUso / 100);
+
     var targetScrollPositionUso = document.getElementById("ciclo_aux_uso").getBoundingClientRect().top + window.scrollY + offsetUso;
 
     window.scrollTo({ top: targetScrollPositionUso, behavior: 'smooth' });
 
 
+      alert('uso')
     
 });
 
@@ -95,11 +132,17 @@ var textPathNuevaVida = document.getElementById("textpath-nuevavida");
 document.getElementById("aux-nuevavida").addEventListener("click", function() {
   
 
-    var offsetNuevaVida = -410; // Adjust this value based on your requirement
-    var targetScrollPositiotNuevaVida = document.getElementById("ciclo_aux_nuevavida").getBoundingClientRect().top + window.scrollY + offsetNuevaVida;
+    var offsetPercentageNV = 10; // 10% of the viewport height
 
-    window.scrollTo({ top: targetScrollPositiotNuevaVida, behavior: 'smooth' });
+    // Calculate the offset based on the viewport height
+    var viewportHeightNV = window.innerHeight;
+    var offsetNuevaVida = -(viewportHeightNV * offsetPercentageNV / 100);
 
+    var targetScrollPositionNuevaVida = document.getElementById("ciclo_aux_nuevavida").getBoundingClientRect().top + window.scrollY + offsetNuevaVida;
+
+    window.scrollTo({ top: targetScrollPositionNuevaVida, behavior: 'smooth' });
+
+      alert('nv')
 
    
 });
